fa30c647fd356a7042c2763e15254f74ff17533e
asmaaHamdym <asmahamdym@gmail.com>
2024-05-19 16:58:46 +0300

    Updated index.html to add Category and Type columns, and script.js to fetch data without specifying a category and to display Category and Type values.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let questionBody = document.querySelector("tbody");
 const triviaQuestion = async () => {
   try {
-    const response = await fetch(
-      "https://opentdb.com/api.php?amount=30&category=23"
-    );
+    const response = await fetch("https://opentdb.com/api.php?amount=30");
     const data = await response.json();
     const newData = data.results;
 
     newData.map((item) => {
       const newTask = document.createElement("tr");
       newTask.innerHTML = `
-            <td>${item.question}</td>
+            <td>${item.category}</td>
+            <td>${item.type}</td>
             <td>${item.difficulty}</td>
+            <td>${item.question}</td>
             <td>${item.correct_answer}</td>
             `;
       questionBody.appendChild(newTask);
